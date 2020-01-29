e63b64a928657a62239497078467f2e69b8ef6b1
Alecqss <60017953+Alecqss@users.noreply.github.com>
2020-01-29 12:26:47 +0100

    Update monjs.js

--- monjs.js
+++ monjs.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+	// $('h1').hide(5000);
+
+	for (var i = 0; i < 10; i++) {
+		$('<div class="col-1">' ).appendTo('#effets');
+	}
+$("#effets div").click(function(){
+	$(this ).hide( 2000,function(){
+		$(this).remove();
+	})
+})
+
+
 
 });
